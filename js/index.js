02e09590cd2255342cb9f4b9afcaad16ffc24c0e
Naok1526 <koizumi.12625@gmail.com>
2022-07-12 04:22:36 +0900

    Add: make humburger-menu and contact page

--- js/index.js
+++ js/index.js
@@ -2,10 +2,27 @@ window.addEventListener('scroll', () => {
   let header = document.getElementById('js-header');
   let section = document.getElementById('js-section');
   let elemTop = section.getBoundingClientRect().top;
-  console.log(elemTop);
+  // console.log(elemTop);
   if (0 >= elemTop) {
     header.classList.add('js-header-bgc');
   } else {
     header.classList.remove('js-header-bgc');
   };
 });
+let hgBtn = document.getElementById('js-button');
+let isOpen = document.getElementById('js-hg-menu');
+
+let clickEnabled = (el) => {
+  el.classList.toggle('close-button');
+  isOpen.classList.toggle('is-open');
+
+  isOpen.onclick = function(){
+    el.classList.remove('close-button');
+    isOpen.classList.remove('is-open');
+  }
+}
+
+//上記関数をボタンクリック時に実行
+hgBtn.addEventListener('click', () => {
+  clickEnabled(hgBtn);
+}, false);
